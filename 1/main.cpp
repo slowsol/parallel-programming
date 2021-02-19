c48fdc298551e87c10095e54c149c6ea1c6636de
slowsol <s.ig.gubin@gmail.com>
2021-02-19 09:13:58 +0300

    Add multiple threading

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <vector>
+#include <chrono>
 
 void ThreadProc(int index)
 {
-	std::cout << "Thread " << index << " executing" << std::endl;
+	std::cout << "Thread " + std::to_string(index) + " executing\n";
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2) {
+	if (argc != 2) 
+	{
 		std::cout << "Invalid arguments count" << std::endl;
 
 		return 1;
 	}
 
+	std::vector<std::thread> threads;
+	
 	int numberOfThreads = std::atoi(argv[1]);
 
 	for (int i = 0; i < numberOfThreads; i++)
 	{
-		std::thread thread(&ThreadProc, i + 1);
-		
+		threads.emplace_back(std::thread(&ThreadProc, i + 1));
+	}
+
+	for (auto& thread : threads)
+	{
 		thread.join();
 	}
 
